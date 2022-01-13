c566801ddf11b19f5a209acbedd1137491114654
Veronica Minciuna <veronicaminci@gmail.com>
2022-01-13 09:57:34 -0500

    config

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBap4WMq7EXenU89vDMAlNNIU4CQ-LPYpU",
+    authDomain: "clone-989c5.firebaseapp.com",
+    projectId: "clone-989c5",
+    storageBucket: "clone-989c5.appspot.com",
+    messagingSenderId: "561185561587",
+    appId: "1:561185561587:web:45cc254cb08f695d61d086"
+  };
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Amazon clone store</h1>
+    </div>
+  );
+}
+
+export default App;
