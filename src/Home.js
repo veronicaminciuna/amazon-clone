8f2d308baf2dd1f870613d4258cc1f05b7f598d7
Veronica Minciuna <veronicaminci@gmail.com>
2022-01-13 19:59:10 -0500

    product component fixed

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Product from "./Product";
+
 function Home() {
   return (
     <div className="home">
@@ -15,11 +16,13 @@ function Home() {
           <Product />
         </div>
         <div className="home_row">
-          {/*Product Component*/}
-          {/*Product Component*/}
-          {/*Product Component*/}
+          <Product />
+          <Product />
+          <Product />
         </div>
-        <div className="home_row">{/*Product Component*/}</div>
+        <div className="home_row">
+          <Product />
+        </div>
       </div>
     </div>
   );
