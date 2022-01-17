c6324c7eea12922728fd30c6ae31eb41e9a8d748
Veronica Minciuna <veronicaminci@gmail.com>
2022-01-17 12:42:07 -0500

    user mail to checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./stateProvider";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout_left">
@@ -14,8 +14,10 @@ function Checkout() {
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
           alt=""
         />
-        <div className="checkout_title">
-          <h2>Your shopping Basket</h2>
+
+        <div>
+          <h3>Hello, {user.email}</h3>
+          <h2 className="checkout_title">Your shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
