7fc0f804475e0c42a46017503ec9f1f4da202549
Veronica Minciuna <veronicaminci@gmail.com>
2022-01-19 00:16:48 -0500

    hide -add to basket- in order

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./stateProvider";
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ basket }, dispatch] = useStateValue();
   const removeFromBasket = () => {
     dispatch({
@@ -26,7 +26,9 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>⭐️</p>
             ))}
         </div>
-        <button onClick={removeFromBasket}>Remove from the Basket</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from the Basket</button>
+        )}
       </div>
     </div>
   );
